962f301bd2c8fbe6f3633428f2c65d307c56fae3
paulgigy <pjngigeh@gmail.com>
2015-06-22 16:25:13 +0300

    first push

---
+++ index.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html>
+<head>
+<!-- linking the .css file -->
+<link rel="stylesheet" type="text/css" href="css/mystyle.css">
+<title>MY WEBSITE</title>
+</head>
+
+<body>
+	<div class="hero">
+		<div class="menu-top">
+			<div class="logo-cover">
+				<div class="real-logo"><img src="images/logo.jpg"></div>
+			</div>
+		</div>
+		
+	</div>
+
+
+</body>
+</html>
